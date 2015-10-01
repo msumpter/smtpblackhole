3655590b9ac6dbe0bf98328b9fea4c5afa31dbcc
Ben Hencke <brainstar@gmail.com>
2015-10-01 11:22:39 -0700

    speed up

--- installblackhole.sh
+++ installblackhole.sh
@@ -4,4 +4,8 @@ postconf -e relayhost=
 postconf -e relay_transport=relay
 postconf -e relay_domains=static:ALL
 postconf -e smtpd_end_of_data_restrictions="check_client_access static:discard"
-
+postconf -e smtp_dns_support_level=disabled
+postconf -e disable_dns_lookups=yes
+postconf -e in_flow_delay=0
+postconf -e smtpd_error_sleep_time=0
+postconf -e smtpd_client_connection_count_limit=0
